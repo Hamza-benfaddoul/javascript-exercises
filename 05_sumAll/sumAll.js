af77dda7902d4239529930e3bca42e5b28e5d59f
Hamza Benfaddoul <hbenfadd@e1r4p12.1337.ma>
2024-01-12 10:38:50 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+	let sum = 0;
+	if (!Number.isInteger(n1) || !Number.isInteger(n2))
+		return "ERROR";
+	if (n1 < 0 || 0 > n2) 
+		return "ERROR";
+	if (n1 > n2) 
+		[n2, n1] = [n1, n2];
+	for(; n1 <= n2; n1++)
+		sum += n1;
+	return sum;
 
 };
 
