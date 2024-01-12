0042105bcdee5525a2c60474707a4f57ab8802e6
Hamza Benfaddoul <hbenfadd@e1r4p12.1337.ma>
2024-01-12 09:43:10 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arry, ...args) {
+	args.forEach(arg => {
+		index = arry.indexOf(arg);
+	   if (index > -1) {
+		   arry.splice(index, 1);
+	   }
 
+   });
+   return arry;
 };
 
 // Do not edit below this line
